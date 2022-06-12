de6fab554906d865e1a75d1d32c028abc75ff0af
opacteam <mike@minisisinc.com>
2022-06-11 19:23:32 -0700

    restyled banner

--- src/components/Banner/Banner.style.jsx
+++ src/components/Banner/Banner.style.jsx
@@ -1,32 +1,28 @@
-import { autocompleteClasses, Box, Button, Container } from "@mui/material";
+import { autocompleteClasses, Box, Button, Container, Typography } from "@mui/material";
 import styled from "@emotion/styled";
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
 import 'swiper/css';
-const BANNER_HEIGHT = '600px';
-export const BannerContainer = styled(Container)(_ => ({
-    padding: '0 !important',
+const BANNER_HEIGHT = '80vh';
+export const BannerContainer = styled(Container)(({ banner }) => ({
+    padding: '90px 20px',
     height: BANNER_HEIGHT,
-    position: 'relative'
-}))
-
-export const BannerBox = styled(Box)(({ banner }) => ({
-    position: 'absolute',
-    top: '0',
+    position: 'relative',
     backgroundImage: `url('${banner}')`,
-    width: "100%",
-    height: BANNER_HEIGHT,
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
     backgroundSize: 'cover',
-    zIndex: '-2',
-    
-}));
+    maxHeight:'800px'
+
+}))
+
+
 
 export const ShadowLayerBox = styled(Box)(_ => ({
     position: 'absolute',
     top: '0',
+    left: '0',
     width: '100%',
     height: BANNER_HEIGHT,
     backgroundColor: 'rgba(12, 12, 12, 0.7)',
@@ -34,38 +30,24 @@ export const ShadowLayerBox = styled(Box)(_ => ({
 
 }))
 
-export const BannerContent = styled(Box)(_ => ({
-  
-    position: 'absolute',
-    textAlign: 'center',
-    top: '100px',
-    left: 0,
-    right: 0,
-    color: 'white',
 
-}))
 
 export const SearchBar = styled(Box)(({ theme }) => ({
-    position: 'absolute',
-    top: '100px',
+   
     height: '40px',
     display: 'flex',
-    top: '100px',
-    left: 0,
-    right: 0,
-    width: '50vw',
+  
+    width: '100%',
     margin: '0 auto',
     boxShadow: '0px 0px 4px 4px rgba(55,55,55,0.6)',
-    [theme.breakpoints.down('md')]: {
-        width: '100%'
-    },
+    
 
     padding: '0'
 }));
 
 export const InputSearch = styled('input')(({ theme }) => ({
     height: '100%',
-    width: '80%',
+    width: '100%',
     '&:focus': {
         outline: 'none',
     }
@@ -74,5 +56,18 @@ export const InputSearch = styled('input')(({ theme }) => ({
 export const SubmitSearch = styled(Button)(_ => ({
     flexGrow: '1',
     borderRadius: 0
+
+}))
+export const SiteHeading = styled(Typography)(({ theme }) => ({
+    fontSize: '80px',
+   textAlign:'left',
+    [theme.breakpoints.down('md')]: {
+        fontSize: '40px',
+    },
+}))
+
+export const SiteDescription = styled(Typography)(({ theme }) => ({
+    fontSize: '16px',
+    textAlign:'left',
 
 }))
--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,32 +1,72 @@
 
 import React from 'react';
 import { TEMPLATE } from '../../constant';
-import { BannerContainer, BannerBox, ShadowLayerBox, BannerContent, SearchBar, InputSearch, SubmitSearch } from './Banner.style';
-import { Typography, Button } from '@mui/material';
+import { BannerContainer, BannerBox, ShadowLayerBox, BannerContent, SearchBar, InputSearch, SubmitSearch, SiteHeading, SiteDescription } from './Banner.style';
+import { Typography, Button, Grid, Paper, InputBase, IconButton, } from '@mui/material';
 import { useSelector } from 'react-redux/';
+import SearchIcon from '@mui/icons-material/Search';
 import { setHeading } from '../../constant'
 const Banner = () => {
 
     const TEMPLATE = useSelector((state) => state.template);
     const { banner, heading, description } = TEMPLATE;
     return (
-        <BannerContainer maxWidth="false" className="bannerContainer">
-            <BannerBox banner={banner}></BannerBox>
-            <BannerContent>
-                <Typography variant="h1" style={{ fontSize: '50px', wordBreak: 'break-word' }}  >
-                    {heading}
-                </Typography>
-                {/*
-                <Typography variant="h2"  >
-                    {description}
-                </Typography> */}
-                <SearchBar>
-                    <InputSearch placeholder='Enter your keyword' />
-                    <SubmitSearch variant="contained">Search</SubmitSearch>
-                </SearchBar>
+        // <BannerContainer maxWidth="false" className="bannerContainer">
+        //     <BannerBox banner={banner}></BannerBox>
+        //     <BannerContent>
+        //         <Typography variant="h1" style={{ fontSize: '50px', wordBreak: 'break-word' }}  >
+        //             {heading}
+        //         </Typography>
+        //         {/*
+        //         <Typography variant="h2"  >
+        //             {description}
+        //         </Typography> */}
+        //         <SearchBar>
+        //             <InputSearch placeholder='Enter your keyword' />
+        //             <SubmitSearch variant="contained">Search</SubmitSearch>
+        //         </SearchBar>
 
-            </BannerContent>
-            <ShadowLayerBox />
+        //     </BannerContent>
+        //     <ShadowLayerBox />
+        // </BannerContainer>
+        <BannerContainer maxWidth={false} className="bannerContainer" banner={banner}>
+            {/* <BannerBox banner={banner}></BannerBox> */}
+            {/* <ShadowLayerBox /> */}
+            <Grid container spacing={2}>
+                <Grid item md={6} sm={12}>
+                    <SiteHeading variant="h1" >
+                        {heading}
+                    </SiteHeading>
+                    <SiteDescription variant="p"  >
+                        {description}
+                    </SiteDescription>
+                </Grid>
+
+            </Grid>
+            <Grid container spacing={2}>
+                <Grid item md={6} sm={12} style={{width:'100%'}}>
+                    {/* <SearchBar>
+                        <InputSearch placeholder='Enter your keyword' />
+                        <SubmitSearch variant="contained">Search</SubmitSearch>
+                    </SearchBar> */}
+                    <Paper
+                        component="form"
+                        sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: '100%', maxWidth:'500px' }}
+                    >
+
+                        <InputBase
+                            sx={{ ml: 1, flex: 1 }}
+                            placeholder="Enter your search"
+                            inputProps={{ 'aria-label': 'enter your search' }}
+                        />
+                        <IconButton type="submit" sx={{ p: '10px' }} aria-label="search">
+                            <SearchIcon />
+                        </IconButton>
+
+                    </Paper>
+
+                </Grid>
+            </Grid>
         </BannerContainer>
     )
 }
